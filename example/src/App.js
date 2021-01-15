bf1a1856a79a12ec2f4539b4d7ba6db3b6795b56
sainw <sai.nawwun@mokkon.com>
2021-01-15 16:28:32 +0630

    add List

---
+++ example/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { List } from 'mokkon-reactjs'
+import 'mokkon-reactjs/dist/index.css'
+
+const App = () => {
+  const pediatricians = [
+    'Michael Lopez',
+    'Sally Tran',
+    'Brian Lu',
+    'Troy Sakulbulwanthana',
+    'Lisa Wellington',
+  ]
+
+  const psychiatrists = [
+    'Miguel Rodriduez',
+    'Cassady Campbell',
+    'Mike Torrence',
+  ]
+  return <div>
+    <div className="root">
+      <div className="listContainer">
+        <List groupName="Pediatricians" members={pediatricians} />
+      </div>
+      <div className="listContainer">
+        <List groupName="Psychiatrists" members={psychiatrists} />
+      </div>
+    </div>  </div>
+}
+
+export default App
---
+++ src/List/List.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '../styles.module.css'
+
+function List({ groupName, members = [] }) {
+    return (
+      <div>
+        <h5>
+          Group: <em>{groupName}</em>
+        </h5>
+        <ul>
+          <p>Members</p>
+          {members.map((member) => (
+            <li key={member}>{member}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+  
+  export default List;
