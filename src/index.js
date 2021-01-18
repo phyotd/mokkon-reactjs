f1208a9ccc2fc9d72d0cf61866addb310c54e706
PhyoThandar <phyothandar394@gmail.com>
2021-01-18 16:27:50 +0630

    field editor and table

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 export { default as List } from './List/List';
+export { default as FieldEditor } from './FieldEditor/FieldEditor';
+export { default as TableTemplate } from './TableTemplate/TableTemplate';
